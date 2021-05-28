971b674b95b7e4c5e4e6f8ee8635d96ef7da6b37
Carlos Garcia A <c.garcia-ar@outlook.com>
2021-05-28 15:45:05 +0200

    Class normalize added for colorbar range around middle point

--- mf/plots.py
+++ mf/plots.py
@@ -310,6 +310,18 @@ def CreatePiston(radius, N):
     area = np.ones((4*N**2,1))*(radius/N)**2*np.pi/4
     return cs, area    
 
+class MidpointNormalize(mpl.colors.Normalize):
+    def __init__(self, vmin, vmax, midpoint=0, clip=False):
+        self.midpoint = midpoint
+        mpl.colors.Normalize.__init__(self, vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        normalized_min = max(0, 1 / 2 * (1 - abs((self.midpoint - self.vmin) / (self.midpoint - self.vmax))))
+        normalized_max = min(1, 1 / 2 * (1 + abs((self.vmax - self.midpoint) / (self.midpoint - self.vmin))))
+        normalized_mid = 0.5
+        x, y = [self.vmin, self.midpoint, self.vmax], [normalized_min, normalized_mid, normalized_max]
+        return np.ma.masked_array(np.interp(value, x, y))
+        
 # def plotFig(filename,fignr=1):
 #     from scipy.io import loadmat
 #     from numpy import size
