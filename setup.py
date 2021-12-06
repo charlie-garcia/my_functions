d2223d25066b3dc4cd0a3e3db3bab45909e4ee94
charlie-garcia <krls_1716@hotmail.com>
2021-12-06 20:58:29 +0100

    Added my_fenics test folder

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='mf',
-    packages=find_packages(include=['mf']),
+    packages=find_packages(include=['mf', 'my_fenics']),
     version='0.1.0',
     description='My personal python library',
     author='Carlos Garcia A.',
