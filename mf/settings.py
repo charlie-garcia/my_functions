0b728cbcc6b66c91de753e5d45b2375176e71fd8
charlie-garcia <krls_1716@hotmail.com>
2022-01-13 02:49:21 +0100

    get matrix size

--- mf/settings.py
+++ mf/settings.py
@@ -18,3 +18,15 @@ def GetMemory():
 def VarSize(variable):
     import sys
     print('Variable size : %.2f MB\n' %(sys.getsizeof(variable)/(1024**2)))
+
+def mat_to_bytes(dim, nfreq, dtype=64, out="GB"):
+    """Calculate the size of a numpy array in bytes.
+    :param nrows: the number of rows of the matrix.
+    :param ncols: the number of columns of the matrix.
+    :param dtype: the size of each element in the matrix. Defaults to 32bits.
+    :param out: the output unit. Defaults to gigabytes (GB)
+    :returns: the size of the matrix in the given unit
+    :rtype: a float
+    """
+    sizes = {v: i for i, v in enumerate("BYTES KB MB GB TB".split())}
+    return dim**2 * nfreq * dtype / 8 / 1024. ** sizes[out]
