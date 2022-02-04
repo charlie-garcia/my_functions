f69e1470f023901bb8c56977d6cb283c05af4532
charlie-garcia <krls_1716@hotmail.com>
2022-02-04 14:25:40 +0100

    EigensolverMembrane à jour

--- mf/membranes.py
+++ mf/membranes.py
@@ -2,23 +2,17 @@ from dolfin import *
 import numpy as np
 
 def EigenSolveMembrane(N, u, v, T, rho_s, bcs_w):
-    c2 = T/rho_s
-    a = inner(grad(u), grad(v))*dx 
-    b = 1/c2*inner(u,v)*dx
+    import time
+    a = T*inner(grad(u), grad(v))*dx 
+    L = rho_s*inner(u,v)*dx
     
     M_ = PETScMatrix()
     assemble(L, tensor = M_)
     
     # Assemble matrices
-	K_ = PETScMatrix()
-	assemble(a, tensor = K_)
-
-	B = PETScMatrix()
-	assemble(b, tensor = M_)
-
-	# Apply boundary conditions
-	bc.apply(A)
-
+    K_ = PETScMatrix()
+    assemble(a, tensor = K_)
+    
     # object is a list or not 
     if isinstance(bcs_w, list): 
         bcs_w  = bcs_w
@@ -32,12 +26,12 @@ def EigenSolveMembrane(N, u, v, T, rho_s, bcs_w):
     tt1 = time.time()
     print('Computing %.f Modes' %N)           
     solver = SLEPcEigenSolver(K_,M_)                            #[𝐾]{𝑈} = 𝜆[𝑀]{𝑈}
-    solver.parameters["solver"]             = "krylov-schur"
+    # solver.parameters["solver"]             = "krylov-schur"
     solver.parameters["problem_type"]       = "gen_hermitian"
     solver.parameters['spectral_transform'] = 'shift-and-invert'
     solver.parameters['spectral_shift']     = 1e-14
     solver.solve(N)
     k = solver.get_number_converged()
     print(' %.f Modes have converged in %.2f secs!' %(k, time.time() - tt1))
-    
+        
     return solver
