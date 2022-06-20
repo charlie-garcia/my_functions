f99e659b750615bd6fa6b85562622c05d4b0c212
carlos <krls_1716@hotmail.com>
2022-06-20 10:50:23 +0200

    Rotation and inertia matrix

--- mf/math.py
+++ mf/math.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.interpolate as inter
+from fenics import *
 # from fenics import *
 
 def integralPath(x,y,u):
@@ -25,3 +26,42 @@ def check_array(x):
 def dbW(signal):
 	sdB = 10*np.log10(np.abs(signal)/10e-12)
 	return sdB
+
+
+def RotationMatrix(theta):
+    return np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+
+def RotatePoint(x,y,theta):
+    X = x*np.cos(theta) - y*np.sin(theta) 
+    Y = x*np.sin(theta) + y*np.cos(theta)
+    return X,Y
+
+def InertiaMatrix(fmesh, rho):   
+    dx = Measure("dx")(domain=fmesh)
+    V = VectorFunctionSpace(fmesh, 'CG', 1)
+    v = interpolate(Expression(('x[0]', 'x[1]'), degree=2), V)
+    
+    Mx = assemble(rho*v[1]*dx) # integrate over y
+    My = assemble(rho*v[0]*dx) # integrate over x
+    
+    x0 = My/assemble(rho*dx)
+    y0 = Mx/assemble(rho*dx)
+    
+    #% Get eigenvalues
+    Ix  = assemble(v[1]**2*rho*dx)
+    Iy  = assemble(v[0]**2*rho*dx)
+    Ixy = -assemble(v[0]*v[1]*rho*dx)
+    
+    N =2
+    # Construct the inertia matrix
+    I = np.zeros((N,N))
+    I[0][0] =  Ix
+    I[1][1] =  Iy
+    I[0][1] = Ixy
+    I[1][0] = Ixy
+    
+    # Get the eigenvalues adn eigenvectors
+    eig = np.linalg.eig(I)
+    mode = np.c_[eig[1][:,0], eig[1][:,1]]
+    
+    return I, x0, y0, mode
